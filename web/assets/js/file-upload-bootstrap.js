106babba4d6435e3a2828a29161fc1be3bca4494
phptuts <noah.php@gmail.com>
2018-03-03 14:03:26 -0800

    Updating to js to handle dynamic elements

--- web/assets/js/file-upload-bootstrap.js
+++ web/assets/js/file-upload-bootstrap.js
@@ -10,7 +10,7 @@ $(function() {
 
     // We can watch for our custom `fileselect` event like this
     $(document).ready( function() {
-        $(':file').on('fileselect', function(event, numFiles, label) {
+        $(document).on('fileselect', ':file', function(event, numFiles, label) {
 
             var input = $(this).parents('.input-group').find(':text'),
                 log = numFiles > 1 ? numFiles + ' files selected' : label;
